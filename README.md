5375c145997e33bb6d4044a4e6edf26cd669810b
MarieCamillePetit <MarieCamillePetit@outlook.com>
2022-12-02 13:25:46 +0100

    Commit first

---
+++ README.md
@@ -0,0 +1,28 @@
+# sqliteapp
+
+Création d'un CRUD
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+
+
+## Installation de Sqflite
+
+Dans le terminal =  flutter pub add sqflite
+                    flutter pub add path_provider
+
+
+Dans mon cas, j'ai du activer le mode dev sur mon pc ( start ms-settings:developers ) et activer le mode dev.
+
+Ne pas oublier de faire "pub get" dans le pubspec.yaml
---
+++ lib/main.dart
@@ -0,0 +1,168 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+
+main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(SqliteApp());
+}
+
+
+class SqliteApp extends StatefulWidget {
+  const SqliteApp({Key? key}) : super(key: key);
+
+  @override
+  State<SqliteApp> createState() => _SqliteAppState();
+}
+
+class _SqliteAppState extends State<SqliteApp> {
+  int? selectedId; // != null
+  final textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        //Création d'une navbar
+        appBar: AppBar(
+          title: TextField(
+            controller: textController,
+          ),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Grocery>>(
+            future: DataBaseHelper.instance.getGroceries(),
+            builder: (BuildContext context, AsyncSnapshot<List<Grocery>> snapshot){
+              if (!snapshot.hasData) {
+                return Center(child: Text('Chargement..'));
+              }
+              return snapshot.data!.isEmpty
+                ? Center(child: Text('Il y a rien dans la BDD gros'))
+              : ListView(
+                children: snapshot.data!.map((grocery) {
+                  return Center(
+                    child: ListTile(
+                      title: Text(grocery.name),
+                      // Quand on appuie sur une donnée, on peux la modifier
+                      onTap: (){
+                        setState(() {
+                          textController.text = grocery.name;
+                          selectedId = grocery.id;
+
+                        });
+                      },
+                      // On maintient pour supprimer
+                      onLongPress: (){
+                        setState(() {
+                          DataBaseHelper.instance.delete(grocery.id!);
+                        });
+                      },
+                    ),
+                  );
+                }).toList(),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.save),
+          onPressed: () async {
+            //Modification
+            selectedId != null
+                ? await DataBaseHelper.instance.update(Grocery(id: selectedId, name: textController.text),
+            )
+            // si on click sur l'icon, on ajoute dans la base de donnée
+            : await DataBaseHelper.instance.add(Grocery(name: textController.text),
+            );
+            //Nettoyage de la zone de texte
+            setState(() {
+              textController.clear();
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Grocery{
+  // int? !=  null
+  final int? id;
+  final String name;
+
+  Grocery({ this.id, required this.name});
+
+  factory Grocery.fromMap(Map<String, dynamic> json) => new Grocery(
+    id: json['id'],
+    name: json['name'],
+  );
+
+  Map<String,dynamic> toMap(){
+    return{
+      'id':id,
+      'name': name,
+    };
+  }
+}
+
+class DataBaseHelper{
+  DataBaseHelper._privateConstructor();
+  static final DataBaseHelper instance = DataBaseHelper._privateConstructor();
+
+  // Si "_database" n'existe pas le "_initDatabase()" vas l'initialiser
+  static Database? _database;
+  Future<Database> get database async => _database??= await _initDatabase();
+
+  // Création de la database
+  Future<Database> _initDatabase() async{
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, 'groceries.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      //onCreate permet de créer la base de donnée si elle existe pas
+      onCreate: _onCreate,
+    );
+  }
+
+  // Création de la fonction _onCreate pour créer la table et son contenue
+  Future _onCreate(Database db, int version) async{
+    await db.execute('''
+      CREATE TABLE groceries(
+        id INTEGER PRIMARY KEY;
+        name TEXT
+      )
+    ''');
+  }
+
+  // Fonction pour récupèrer le contenue d'une table
+  Future<List<Grocery>> getGroceries() async{
+    Database db = await instance.database;
+    var groceries = await db.query('groceries', orderBy:'name');
+        List<Grocery> groceryList = groceries.isNotEmpty
+          ? groceries.map((c) => Grocery.fromMap(c)).toList()
+            :[];
+    return groceryList;
+  }
+  //Fonction pour ajouter dans la base de donnée
+  Future<int> add(Grocery grocery) async{
+    Database db = await instance.database;
+    return await db.insert('groceries', grocery.toMap());
+  }
+
+  //Fonction pour supprimer dans la base de donnée
+  Future<int> delete(int id) async{
+    Database db = await instance.database;
+    return await db.delete('groceries', where: 'id = ?', whereArgs: [id]);
+  }
+
+  //Fonction pour modifier dans la base de donnée
+  Future<int> update(Grocery grocery) async{
+    Database db = await instance.database;
+    return await db.update('groceries', grocery.toMap(), where: 'id = ?', whereArgs: [grocery.id]);
+  }
+}
